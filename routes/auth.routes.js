4e017d936d05a1bb6a80fdc765a51f71c7c9ab11
appfilme644 <felipemacz16f@gmail.com>
2025-08-22 20:36:15 -0300

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,8 +2,10 @@ import { Router } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/user.js';
+import { OAuth2Client } from "google-auth-library";
 
 const router = Router();
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
 
 // Registro de usuário
 router.post('/register', async (req, res) => {
@@ -53,6 +55,50 @@ router.post('/login', async (req, res) => {
     });
   } catch (err) {
     return res.status(500).json({ message: 'Erro no login', error: err.message });
+  }
+});
+
+router.post("/google", async (req, res) => {
+  try {
+    const { idToken } = req.body;
+
+    if (!idToken) return res.status(400).json({ message: "Token não enviado" });
+
+    // 1. Verifica o token com a lib oficial
+    const ticket = await client.verifyIdToken({
+      idToken,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+
+    const payload = ticket.getPayload();
+
+    // 2. Pega os dados do usuário
+    const { sub, email, name, picture } = payload;
+
+    // 3. Verifica se usuário já existe no banco
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      user = await User.create({
+        nome: name,
+        email,
+        googleId: sub,
+        avatar: picture,
+        role: "user",
+      });
+    }
+
+    // 4. Cria token JWT da sua API
+    const token = jwt.sign(
+      { id: user._id, email: user.email, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    return res.json({ token, user });
+  } catch (err) {
+    console.error("Erro Google Login:", err);
+    return res.status(401).json({ message: "Token inválido" });
   }
 });
 
