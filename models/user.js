79c3454497c526928fe8734589a3f23276412fd2
appfilme644 <felipemacz16f@gmail.com>
2025-08-22 20:12:41 -0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -5,9 +5,11 @@ const userSchema = new mongoose.Schema(
     nome: { type: String, required: true },
     email: { type: String, required: true, unique: true, lowercase: true },
     senhaHash: { type: String, required: true },
-    role: { type: String, enum: ['user', 'admin'], default: 'user' }
+    role: { type: String, enum: ['user', 'admin'], default: 'user' },
+      favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
   },
   { timestamps: true }
 );
 
 export default mongoose.model('User', userSchema);
+
